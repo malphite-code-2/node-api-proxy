3c3c1ffde84cf74c9624d011589bf277a1a0518f
malphite-code-2 <168648509+malphite-code-2@users.noreply.github.com>
2024-05-02 12:26:19 +0000

    init

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const axios = require('axios');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const getRVNbalance = (address) => new Promise(async (resolve) => {
+    try {
+        const response = await axios.get(`https://ravencoin.atomicwallet.io/api/v2/address/${address}`);
+        const data = response.data;
+        resolve(Number(data?.balance || 0) / 100000000)
+    } catch (error) {
+        resolve(0);
+    }
+})
+
+const getBTCbalance = (address) => new Promise(async (resolve) => {
+    try {
+        const response = await axios.get(`https://bitcoin.atomicwallet.io/api/v2/address/${address}`);
+        const data = response.data;
+        resolve(Number(data?.balance || 0) / 100000000)
+    } catch (error) {
+        resolve(0);
+    }
+})
+
+const methods = {
+    RVN: getRVNbalance,
+    BTC: getBTCbalance,
+}
+
+function proxyMain(ws, req) {
+  ws.on('message', (message) => {
+    const command = JSON.parse(message);
+    
+    if(command.method === 'wallet_getBalance') {
+        const id = command.id;
+        const { address, coin } = command.params || null;
+
+        if (!address || !coin) {
+            ws.send(JSON.stringify({ id, address, coin, balance: -1 }));
+            return;
+        };
+
+        methods[coin](address).then(balance => {
+            ws.send(JSON.stringify({ id, address, coin, balance }));
+        })
+    }
+  });
+}
+
+wss.on('connection', proxyMain);
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
